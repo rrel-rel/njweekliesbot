b369b72c6f95280a0a55c2aba1ff3d3c3c5b2787
moon-scape <breamstatue@gmail.com>
2024-01-07 17:58:49 -0500

    added ability to check other people's mmr

--- commands/weeklies/mmr.js
+++ commands/weeklies/mmr.js
@@ -5,28 +5,63 @@ const db = require('../../db.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('mmr')
-        .setDescription('Check your MMR.'),
+        .setDescription('Check your MMR.')
+        .addStringOption(option =>
+            option
+                .setName('username')
+                .setDescription('osu!')
+                .setRequired(false)),
     async execute(interaction) {
         const discordUserId = interaction.user.id;
-
-        db.query('SELECT MMR FROM Player WHERE discord_id = ?', [discordUserId], (err, results) => {
-            if (err) {
-                console.error('Error querying the database:', err);
-                interaction.reply({ content: 'There was an error while checking your MMR.', ephemeral: true });
-                return;
+        const osuUsername = interaction.options.getString('username');
+        if(!(interaction.options.getString('username'))) {
+            try {
+                db.query('SELECT MMR FROM Player WHERE discord_id = ?', [discordUserId], (err, results) => {
+                    if (err) {
+                        console.error('Error querying the database:', err);
+                        interaction.reply({ content: 'There was an error while checking your MMR.', ephemeral: true });
+                        return;
+                    }
+        
+                    if (results.length === 0) {
+                        interaction.reply({ content: 'Your MMR is not set.', ephemeral: true });
+                    } else {
+                        const mmr = results[0].MMR;
+                        const embed = new EmbedBuilder()
+                            .setTitle('MMR')
+                            .setDescription(`Your MMR is: ${mmr}`)
+                            .setColor(0x0099FF);
+                        interaction.reply({ embeds: [embed] });
+                    }
+                });
+            } catch (error) {
+                console.error('Error updating user mmr: ', error);
+                await interaction.reply('An error occurred while updating user mmr');
             }
-
-            if (results.length === 0) {
-                interaction.reply({ content: 'Your MMR is not set.', ephemeral: true });
-            } else {
-                const mmr = results[0].MMR;
-                const embed = new EmbedBuilder()
-                    .setTitle('Current MMR')
-                    .setDescription(`Your MMR is: ${mmr}`)
-                    .setColor(0x0099FF); // You can choose a different color
-
-                interaction.reply({ embeds: [embed] });
+        } else {
+            try {
+                db.query('SELECT MMR FROM Player WHERE username = ?', [osuUsername], (err, results) => {
+                    if (err) {
+                        console.error('Error querying the database:', err);
+                        interaction.reply({ content: `There was an error while checking ${osuUsername}'s MMR.`, ephemeral: true });
+                        return;
+                    }
+        
+                    if (results.length === 0) {
+                        interaction.reply({ content: `${osuUsername}'s MMR is not set.`, ephemeral: true });
+                    } else {
+                        const mmr = results[0].MMR;
+                        const embed = new EmbedBuilder()
+                            .setTitle('MMR')
+                            .setDescription(`${osuUsername}'s MMR is: ${mmr}`)
+                            .setColor(0x0099FF);
+                        interaction.reply({ embeds: [embed] });
+                    }
+                });
+            } catch (error) {
+                console.error('Error updating user mmr: ', error);
+                await interaction.reply('An error occurred while updating user mmr');
             }
-        });
+        }
     }
 };
